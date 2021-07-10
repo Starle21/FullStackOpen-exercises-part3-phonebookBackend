a64f739207fcbb7fa3bffeeaffdf36f46922dbc4
Starle21 <staronova.lenka@gmail.com>
2021-07-10 11:07:20 +0200

    Part 3.3 Phonebook backend step3 get single resource

--- index.js
+++ index.js
@@ -22,10 +22,26 @@ let persons = [
     name: "Mary Poppendieck",
     number: "39-23-6423122",
   },
+  {
+    id: 5,
+    name: "Gaga Poppendieck",
+    number: "6423122",
+  },
 ];
 
 app.get("/api/persons", (request, response) => {
   response.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
 });
 
 app.get("/info", (request, response) => {
