f50bb4ea8d8dd898e50f35778978de8f670fe36c
Starle21 <staronova.lenka@gmail.com>
2021-07-10 19:21:57 +0200

    Part 3.11 Phonebook fullstack step11 build

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const cors = require("cors");
 const morgan = require("morgan");
 const app = express();
 
+app.use(express.static("build"));
 app.use(cors());
 app.use(express.json());
+
 // Morgan using predefined configuration
 // app.use(morgan("tiny"));
 
